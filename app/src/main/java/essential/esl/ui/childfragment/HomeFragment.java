7d6d96d02b1107cbd5b27850602ac40c9619103a
hoabackz <hoaback@gmail.com>
2016-05-19 00:19:29 +0700

    Home- Onclick Animation

--- app/src/main/java/essential/esl/ui/childfragment/HomeFragment.java
+++ app/src/main/java/essential/esl/ui/childfragment/HomeFragment.java
@@ -2,14 +2,18 @@ package essential.esl.ui.childfragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import essential.esl.R;
 import essential.esl.app.BaseChildFragment;
+import essential.esl.app.MyAnimation;
 
 /**
  * Created by admin on 5/18/2016.
  */
-public class HomeFragment  extends BaseChildFragment{
+public class HomeFragment extends BaseChildFragment implements View.OnClickListener {
+    private LinearLayout btn1, btn2, btn3;
+
     @Override
     protected int getLayoutResIdContentView() {
         return R.layout.fragment_home;
@@ -17,6 +21,30 @@ public class HomeFragment  extends BaseChildFragment{
 
     @Override
     protected void onCreateContentView(View rootView, Bundle savedInstanceState) {
+        init(rootView);
+    }
 
+    public void init(View rootView) {
+        btn1 = (LinearLayout) rootView.findViewById(R.id.btn_lv1);
+        btn2 = (LinearLayout) rootView.findViewById(R.id.btn_lv2);
+        btn3 = (LinearLayout) rootView.findViewById(R.id.btn_lv3);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        MyAnimation.animZoomWhenOnClick(v, HomeFragment.this, 1, 1.1f, 1, 1.1f);
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_lv1:
+                break;
+            case R.id.btn_lv2:
+                break;
+            case R.id.btn_lv3:
+                break;
+        }
     }
 }
